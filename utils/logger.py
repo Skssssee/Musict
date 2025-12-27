d0aa628921dae7c9ce9c9874405aa8ed6de7cffe
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-12-27 19:03:58 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,17 @@
+# utils/logger.py
+
+import logging
+import os
+
+os.makedirs("logs", exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(message)s",
+    handlers=[
+        logging.FileHandler("logs/musicbot.log"),
+        logging.StreamHandler()
+    ]
+)
+
+LOGGER = logging.getLogger("MusicBot")
